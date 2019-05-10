ffbefe79a2eb3dbc43c330d48577506fc2d06319
Martin Laptop <martin01.shen@gmail.com>
2019-05-10 09:19:52 +0200

    Version 0.0.1
    Added Plane
    Added Ball
    Added Movement Script (unfinished)

---
+++ Brawl Ball/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float player1spd = 5;
+    Rigidbody myRigidbody;
+    float h;
+    float v;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        h = Input.GetAxis("Horizontal");
+    }
+}
